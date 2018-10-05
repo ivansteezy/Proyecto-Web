e9897f252f2187c2980785d1d4eb6a6808fe3510
Spaderaven <watsit777@gmail.com>
2018-10-04 23:06:20 -0500

    Productos TECH N9NE

---
+++ Proyecto_Web/HipHop.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rap</title>
+
+	<link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/Productos.css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+	<div class="jumbotron">
+      <h1>Sample Text.</h1>
+    </div>
+
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+          <div class="collapse navbar-collapse" id="collapse_target">
+              <ul class="nav navbar-nav">
+
+                <li class="nav-item dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle = "dropdown">Productos</a>
+                    <ul class="dropdown-menu">
+
+                      <li><a href="HipHop.php">Hip Hop</a></li>
+                      <li><a href="Pop.php">Pop</a></li>
+                      <li><a href="Rock.php">Rock</a></li>
+                      <li><a href="Electro.php">Electro</a></li>
+                      <li><a href="Oldies.php">Oldies</a></li>
+
+                    </ul>
+               </li>
+               
+                <li class="nav-item">
+                  <a href="CrearCuenta.php" class="nav-link">Nosotros</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">Opcion 3</a>
+                </li>
+                <li class="nav-item">
+                  <a href="Login.php" class="nav-link">Login</a>
+                </li>
+              </ul>
+          </div>
+        </nav>
+
+
+
+
+
+
+
+	<div class="container">
+
+
+		<div class="row">
+
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+
+				<a href="" style="text-decoration: none">
+					<div class="thumnail">
+						
+						<img src="Imagenes/TechN9ne.jpg" style="width: 150px; height: 150px;">
+
+						<div class="caption" style="padding-left: 20px">
+							<h2>Planet</h2>
+							<h3>Tech N9ne</h3>
+						</div>
+
+					</div>
+				</a>
+
+			</div>
+
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+
+				<a href="" style="text-decoration: none">
+					<div class="thumnail">
+						
+						<img src="Imagenes/TechN9ne.jpg" style="width: 150px; height: 150px;">
+
+						<div class="caption" style="padding-left: 20px">
+							<h2>Planet</h2>
+							<h3>Tech N9ne</h3>
+						</div>
+
+					</div>
+				</a>
+
+			</div>
+
+
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+
+				<a href="" style="text-decoration: none">
+					<div class="thumnail">
+						
+						<img src="Imagenes/TechN9ne.jpg" style="width: 150px; height: 150px;">
+
+						<div class="caption" style="padding-left: 20px">
+							<h2>Planet</h2>
+							<h3>Tech N9ne</h3>
+						</div>
+
+					</div>
+				</a>
+
+			</div>
+
+
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+
+				<a href="" style="text-decoration: none" >
+					<div class="thumnail">
+						
+						<img src="Imagenes/TechN9ne.jpg" style="width: 150px; height: 150px;">
+
+						<div class="caption" style="padding-left: 20px">
+							<h2>Planet</h2>
+							<h3>Tech N9ne</h3>
+						</div>
+
+					</div>
+				</a>
+
+			</div>
+
+
+			
+		</div>
+
+		
+
+
+
+	</div>
+
+</body>
+</html>
